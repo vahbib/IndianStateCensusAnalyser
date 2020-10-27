d19565c3a1b3eac463f215000dd60b92760c2a16
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-27 22:32:33 +0530

    Given the State Code CSV File when correct but csv header incorrect Returns a StateException

--- src/test/java/com/cg/StateCodeTest.java
+++ src/test/java/com/cg/StateCodeTest.java
@@ -1,4 +1,20 @@
 package com.cg;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class StateCodeTest {
+    @Test
+    public void GivenTheStateCodeCsvFile_HeaderIncorrect_ShouldThrowStateException() throws IOException {
+        final String STATECODE_CSVFILE= "F:\\Java_Workspace\\StateCensusData.csv";
+        final String WRONG_FILE = "/wrong.txt";
+        try {
+            int count = StateCode.openCsvBuilder(STATECODE_CSVFILE, StateCode.class);
+        } catch (StateException e) {
+            e.printStackTrace();
+            Assert.assertEquals(StateException.StateExceptionType.INCORRECT_DATA_ISSUE, e.type);
+        }
+    }
 }
