0235b61833fdb525ad2762eeec3d9e6ff6146653
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-27 22:14:57 +0530

    Given the State Code CSV File when correct but typw incorrect should return StateException

--- src/test/java/com/cg/StateCodeTest.java
+++ src/test/java/com/cg/StateCodeTest.java
@@ -1,4 +1,20 @@
 package com.cg;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class StateCodeTest {
+    @Test
+    public void GivenTheStateCensusCsvFile_FileExtIncorrect_ShouldThrowStateException() throws IOException {
+        final String STATECENSUS_CSVFILE= "F:\\Java_Workspace\\StateCensusData.csv";
+        final String WRONG_FILE = "/wrong.txt";
+        try {
+            int count = StateCode.openCsvBuilder(STATECENSUS_CSVFILE, StateCodeAnalyser.class);
+        } catch (StateException e) {
+            e.printStackTrace();
+            Assert.assertEquals(StateException.StateExceptionType.INCORRECT_DATA_ISSUE, e.type);
+        }
+    }
 }
