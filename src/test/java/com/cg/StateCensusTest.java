bfd5dc8f9ccc438b339d75c0c7c670991d47c01f
Bibhav Singh <71620937+vahbib@users.noreply.github.com>
2020-10-27 17:25:05 +0530

    Check to ensure the Number of Record matches

--- src/test/java/com/cg/StateCensusTest.java
+++ src/test/java/com/cg/StateCensusTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class StateCensusTest {
-    public static final String STATECENSUS_CSVFILE= "F:\\Java_Workspace\\StateCensusData.csv";
-    public static final String WRONG_FILE = "/wrong.txt";
+    final String STATECENSUS_CSVFILE= "F:\\Java_Workspace\\StateCensusData.csv";
+    final String WRONG_FILE = "/wrong.txt";
     @Test
     public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
         try {
-            int count = StateCensus.openCsvBuilder(STATECENSUS_CSVFILE, StateCensus.class);
+            int count = StateCensus.openCsvBuilder(STATECENSUS_CSVFILE, StateCensusAnalyser.class);
             System.out.println(count);
             Assert.assertEquals(29, count);
         } catch (StateException e) {
