c24edeee918100528d384e60e070a85cf125e9d2
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-27 22:29:10 +0530

    Given the State Code CSV File when correct but type Delimiter incorrect should return StateException

--- src/main/java/com/cg/StateCodeAnalyser.java
+++ src/main/java/com/cg/StateCodeAnalyser.java
@@ -1,4 +1,117 @@
 package com.cg;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Iterator;
 
 public class StateCodeAnalyser {
+
+
+    @CsvBindByName(column = "SrNo")
+    private String srNo;
+
+    @CsvBindByName(column = "State",required = true)
+    private String state;
+
+    @CsvBindByName(column = "Name")
+    private String Name;
+
+    @CsvBindByName(column = "TIN", required = true)
+    private String Tin;
+
+    public StateCodeAnalyser() {
+
+    }
+
+    public String getSrNo() {
+        return srNo;
+    }
+
+    public void setSrNo(String srNo) {
+        this.srNo = srNo;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state=state;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setTin(String Tin) {
+        this.Tin = Tin;
+    }
+    public String getTin() {
+        return Tin;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return  "srNo='" + srNo + '\'' +
+                ", StateName='" + state + '\'' +
+                ", Name='" + Name + '\'' +
+                ", TIN ='" + Tin + '\'' +"\n";
+    }
+}
+
+class StateCode {
+
+    public StateCode() {
+    }
+
+    public static <E>  int openCsvBuilder(String csvFilePath, Object myClass) throws StateException {
+        int counter = 0;
+        try {
+            Iterator<Object> myIterator = getIterator(csvFilePath, myClass);
+            while ( myIterator.hasNext() ) {
+                counter++;
+                Object myObj = myIterator.next();
+            }
+        } catch (StateException e){
+            throw e;
+        } catch (RuntimeException e){
+            throw new StateException(StateException.StateExceptionType.DELIMITER_ISSUE,
+                    "Error related to delimiter at record no. : " +(counter+1));
+        }
+        return counter;
+    }
+
+    public static Iterator<Object> getIterator(String csvFilePath, Object myClass) throws StateException {
+        Reader reader = null;
+        CsvToBean<Object> csvToBean;
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            csvToBean = new CsvToBeanBuilder(reader).withType((Class) myClass).withIgnoreLeadingWhiteSpace(true).build();
+            Iterator<Object> iter= csvToBean.iterator();
+            return iter;
+        } catch (NoSuchFileException e) {
+            throw new StateException(StateException.StateExceptionType.NO_SUCH_FILE,
+                    "file doesn't exists, Enter correct file");
+        } catch (RuntimeException e){
+            throw new StateException(StateException.StateExceptionType.INCORRECT_DATA_ISSUE,
+                    "delimiter error at line 1" +
+                            "related to headers or incorrect extension / input file ");
+        } catch (IOException e) {
+            throw new StateException(StateException.StateExceptionType.SOME_OTHER_IO_EXCEPTION,
+                    "Some other IO related exception");
+        }
+    }
+
 }
--- src/test/java/com/cg/StateCodeTest.java
+++ src/test/java/com/cg/StateCodeTest.java
@@ -1,4 +1,20 @@
 package com.cg;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class StateCodeTest {
+    @Test
+    public void GivenTheStateCodeCsvFile_DelimiterIncorrect_ShouldThrowStateException() throws IOException {
+        final String STATECODE_CSVFILE= "F:\\Java_Workspace\\StateCode.csv";
+        final String WRONG_FILE = "/wrong.txt";
+        try {
+            int count = StateCode.openCsvBuilder(STATECODE_CSVFILE, StateCodeAnalyser.class);
+        } catch (StateException e) {
+            e.printStackTrace();
+            Assert.assertEquals(StateException.StateExceptionType.DELIMITER_ISSUE, e.type);
+        }
+    }
 }
