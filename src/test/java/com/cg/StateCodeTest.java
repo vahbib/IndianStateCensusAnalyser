6da21f54e99f80759b720a7a08babb9c32d28751
Bibhav Singh <71620937+vahbib@users.noreply.github.com>
2020-10-27 22:24:43 +0530

    Given the State Code CSV File when correct but type incorrect Returns a StateException

--- src/test/java/com/cg/StateCodeTest.java
+++ src/test/java/com/cg/StateCodeTest.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 
 public class StateCodeTest {
     @Test
-    public void GivenTheStateCensusCsvFile_FileExtIncorrect_ShouldThrowStateException() throws IOException {
-        final String STATECENSUS_CSVFILE= "F:\\Java_Workspace\\StateCensusData.csv";
+    public void GivenTheStateCodeCsvFile_FileExtIncorrect_ShouldThrowStateException() throws IOException {
+        final String STATECODE_CSVFILE= "F:\\Java_Workspace\\StateCensusData.csv";
         final String WRONG_FILE = "/wrong.txt";
         try {
             int count = StateCode.openCsvBuilder(STATECENSUS_CSVFILE, StateCodeAnalyser.class);
