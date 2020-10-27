111e36c309eef1ecef453f7c8edb36714e50d292
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-27 22:11:36 +0530

    Given the State Code CSV File if incorrect Returns a custom Exception

--- src/test/java/com/cg/StateCodeTest.java
+++ src/test/java/com/cg/StateCodeTest.java
@@ -1,4 +1,24 @@
 package com.cg;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class StateCodeTest {
+
+
+
+
+        @Test
+        public void GivenStateCensusCsvFile_If_DoesntExist_ShouldThrowStateException() throws IOException {
+            final String STATECENSUS_CSVFILE= "F:\\Java_Workspace\\StateCensusData.csv";
+            final String WRONG_FILE = "/wrong.txt";
+            try {
+                StateCode.openCsvBuilder(WRONG_FILE, StateCodeAnalyser.class);
+            } catch (StateException e) {
+                e.printStackTrace();
+                Assert.assertEquals(StateException.StateExceptionType.NO_SUCH_FILE, e.type);
+            }
+        }
 }
