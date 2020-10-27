00dada2535f845ac906b4b094b48999c212e76c2
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-27 22:05:15 +0530

    STATE CODE: Check to ensure the number of record matches

--- src/test/java/com/cg/StateCodeTest.java
+++ src/test/java/com/cg/StateCodeTest.java
@@ -1,4 +1,21 @@
 package com.cg;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class StateCodeTest {
+    @Test
+    public void GivenStateCodesCsvFile_IfCorrectNumOfRecords_Should_ReturnTrue() throws IOException {
+        final String STATECODE_CSVFILE = "F:\\Java_Workspace\\StateCode.csv";
+        final String WRONGFILE = "/wrong.csv";
+        try {
+            int count = StateCode.openCsvBuilder(STATECODE_CSVFILE, StateCodeAnalyser.class);
+            System.out.println(count);
+            Assert.assertEquals(27, count);
+        } catch (StateException e) {
+            e.printStackTrace();
+        }
+    }
 }
